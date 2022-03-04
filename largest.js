cf6fd961d7e633eecddc892deb9acf7383075f79
Anu Ale Magar <anumagar354@gmail.com>
2022-03-04 20:36:12 +0545

    this is first commit

---
+++ largest.js
@@ -0,0 +1,12 @@
+let a=3;
+let b=8;
+let c=10;
+if(a>b && a>c){
+    console.log("a is largest")
+}
+else if(b>a && b>c){
+    console.log("b is largest");
+}
+else{
+    console.log("c is largest");
+}
